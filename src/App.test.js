d132fabbc4f1f12d44a815319c25d8f242e58e18
Pham Trang <trang@Phams-MacBook-Pro.local>
2022-02-20 14:34:06 +0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react 1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello Trang/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello Trang/i);
   expect(linkElement).toBeInTheDocument();
 });
